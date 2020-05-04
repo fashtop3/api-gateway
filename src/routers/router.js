b3c1d88773a6f2515a7f13e0510d0560e10e70d4
Fashola Ayodeji <fashtop3@gmail.com>
2020-05-04 21:30:36 +0100

    do clean

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,16 +4,7 @@ var router = express.Router();
 // const hashtagRouter = require('./hashtagService');
 // const authRouter = require('../controller/AuthController');
 
-const routeServices = [
-  './commonService',
-  './authService',
-  './hotelService',
-  './customerService',
-  './conciergeService',
-  './restaurantService',
-  // 'hashtagService'
-]
-
+const routeServices = ['./bootstrap',]
 
 router.use((req, res, next) => {
   console.log("Called: ", `[${req.method}]`, req.path);
