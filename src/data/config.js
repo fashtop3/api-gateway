69a10bb38fd42dd64499c11dd15cbb6a58bf6f01
Fashola Ayodeji <fashtop3@gmail.com>
2020-06-04 13:49:54 +0100

    updates for dev

---
+++ src/data/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // loan_ms: "http://loan:5000",
+  loan_ms: "https://services.dev.lever.ng/_api/loan",
+  // wallet_ms: "http://developer.lever.ng:8082",
+  // wallet_ms: "https://wallets.lever.ng",
+  // wallet_ms: "http://wallet:8085",
+  wallet_ms: "https://services.dev.lever.ng/_api/wallet",
+  users_ms: "http://users:5000",
+  // users_ms: "https://services.dev.lever.ng/_api/users",
+  // users_ms: "http://192.168.99.100:8006",
+  business_ms: "http://businessapi:5000/api/v1",
+  // business_ms: "https://services.dev.lever.ng/_api/business",
+  // business_ms: "http://192.168.99.100:8007",
+}
